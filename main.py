b3ae8face9a409d861ec9fe91ff0aaae4df3e574
bonlilicheng <bonlilicheng916@gmail.com>
2023-12-17 20:48:32 -0500

    CICD4

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import openai
-import os
-from flask import render_template
 
 # Initialize Flask app
 app = Flask(__name__)
 
 # Function to query OpenAI API
-
 openai.api_key = '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'
 def query_openai(prompt):
     try:
@@ -23,10 +20,10 @@ def query_openai(prompt):
 
 
 # Home route
-# Home route
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 # Route to handle question asking
 @app.route('/ask', methods=['POST'])
@@ -39,32 +36,19 @@ def ask():
     return jsonify({"answer": answer})
 
 
-
 @app.route('/chat')
 def chat():
     return app.send_static_file('index.html')
 
+
 data_store = {
-    1: "Sample question 1",
-    2: "Sample question 2",
-    3: "Sample question 3",
-    4: "Sample question 4",
-    5: "Sample question 5",
-    6: "Sample question 6",
-    7: "Sample question 7",
-    8: "Sample question 8",
-    9: "Sample question 9",
-    10: "Sample question 10",
-    11: "Sample question 11",
-    12: "Sample question 12",
-    13: "Sample question 13",
-    14: "Sample question 14",
-    15: "Sample question 15",
-    16: "Sample question 16",
-    17: "Sample question 17",
-    18: "Sample question 18",
-    19: "Sample question 19",
-    20: "Sample question 20"
+    1: "Sample question 1", 2: "Sample question 2", 3: "Sample question 3",
+    4: "Sample question 4", 5: "Sample question 5", 6: "Sample question 6",
+    7: "Sample question 7", 8: "Sample question 8", 9: "Sample question 9",
+    10: "Sample question 10", 11: "Sample question 11", 12: "Sample question 12",
+    13: "Sample question 13", 14: "Sample question 14", 15: "Sample question 15",
+    16: "Sample question 16", 17: "Sample question 17", 18: "Sample question 18",
+    19: "Sample question 19", 20: "Sample question 20"
 }
 
 
@@ -77,6 +61,7 @@ def update_question(question_id):
     data_store[question_id] = question_data
     return jsonify({"message": "Question updated successfully."})
 
+
 @app.route('/questions/<int:question_id>', methods=['DELETE'])
 def delete_question(question_id):
     if question_id not in data_store:
@@ -84,6 +69,7 @@ def delete_question(question_id):
 
     del data_store[question_id]
     return jsonify({"message": "Question deleted successfully."})
+
 
 @app.route('/questions', methods=['GET'])
 def list_questions():
@@ -95,8 +81,6 @@ def list_questions():
     return jsonify(paginated_data)
 
 
-
 # Main execution
 if __name__ == '__main__':
-    # Run the Flask app
     app.run(debug=True)
