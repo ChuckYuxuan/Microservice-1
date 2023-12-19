c4a17f3acebc14737c2da2c378583feca29ec3d3
Noah Zhou <zz29002023@163.com>
2023-12-19 07:46:37 -0500

    add graphql

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect
+from flask_graphql import GraphQLView
+
 from dbconnector import get_db_conn, close_conn, init_db, mock_data
 from resources.score_resource import ScoreResource
+from resources.graphql_schema import schema
 
 app = Flask(__name__)
 instance = ScoreResource()
@@ -16,7 +19,6 @@ async def search_student_async():
         id = request.form.get('student_id')
         result = await instance.get_score_async(id)
         if result:
-            print(result)
             return render_template('search_student.html', results=result['score']['score'], info=result['student']['student'])
 
     return render_template('search_student.html')
@@ -28,43 +30,9 @@ async def search_student_sync():
         id = request.form.get('student_id')
         result = await instance.get_score_sync(id)
         if result:
-            print(result)
             return render_template('search_student.html', results=result['score']['score'], info=result['student']['student'])
 
     return render_template('search_student.html')
-
-
-@app.route('/api/dashboard/view_score')
-def view_score():
-    return render_template('view_score.html')
-
-
-@app.route('/insert_scores', methods=['POST'])
-def add_score():
-    student_id = request.json.get('student_id')
-    student_name = request.json.get('student_name')
-    course_id = request.json.get('course_id')
-    course_name = request.json.get('course_name')
-    score = request.json.get('score')
-
-    conn = get_db_conn()
-    conn.execute(f"INSERT INTO scores VALUES ({student_id}, '{student_name}', {course_id}, '{course_name}', {score})")
-    close_conn(conn)
-
-    return '', 201
-
-
-@app.route('/average_scores', methods=['POST'])
-def get_average_scores():
-    course_id = request.json.get('course_id')
-
-    conn = get_db_conn()
-    result = conn.execute(f"SELECT year, AVG(score) FROM history_scores WHERE course_id = {course_id} GROUP BY year")
-    scores = result.fetchall()
-    conn.close()
-
-    return jsonify(scores)
-
 
 @app.route('/get_score_info_json/<id>')
 def get_score_info(id):
@@ -83,6 +51,44 @@ def get_student_json(id):
     close_conn(conn)
     return jsonify({'student': student})
 
+
+@app.route('/insert_scores', methods=['POST'])
+def add_score():
+    student_id = request.json.get('student_id')
+    student_name = request.json.get('student_name')
+    course_id = request.json.get('course_id')
+    course_name = request.json.get('course_name')
+    score = request.json.get('score')
+
+    conn = get_db_conn()
+    conn.execute(f"INSERT INTO scores VALUES ({student_id}, '{student_name}', {course_id}, '{course_name}', {score})")
+    close_conn(conn)
+
+    return '', 201
+
+
+@app.route('/api/dashboard/view_score')
+def view_score():
+    return render_template('view_score.html')
+
+
+@app.route('/average_scores', methods=['POST'])
+def get_average_scores():
+    course_id = request.json.get('course_id')
+
+    conn = get_db_conn()
+    result = conn.execute(f"SELECT year, AVG(score) FROM history_scores WHERE course_id = {course_id} GROUP BY year")
+    scores = result.fetchall()
+    conn.close()
+
+    return jsonify(scores)
+
+# use graphql to search student scores
+app.add_url_rule('/api/dashboard/graphql_search_student', view_func=GraphQLView.as_view(
+    'graphql',
+    schema=schema,
+    graphiql=True,
+))
 
 if __name__ == '__main__':
     init_db()
